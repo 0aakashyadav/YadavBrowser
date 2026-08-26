56dc740911140b422ed8a8afb98b758ce3835808
Aakash Yadav <98048507+0aakashyadav@users.noreply.github.com>
2026-08-26 12:26:20 +0530

    Connect AARYA securely to YadavBrowser

--- preload.js
+++ preload.js
@@ -2,41 +2,24 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("browserAPI", {
   navigate: (url) => ipcRenderer.send("navigate", url),
-
   back: () => ipcRenderer.send("back"),
-
   forward: () => ipcRenderer.send("forward"),
-
   reload: () => ipcRenderer.send("reload"),
-
   newTab: () => ipcRenderer.send("new-tab"),
-
   closeTab: () => ipcRenderer.send("close-tab"),
-
-  switchTab: (direction) =>
-    ipcRenderer.send("switch-tab", direction),
-
-  focusAddressBar: () =>
-    ipcRenderer.send("focus-address-bar"),
+  switchTab: (direction) => ipcRenderer.send("switch-tab", direction),
+  focusAddressBar: () => ipcRenderer.send("focus-address-bar"),
+  aaryaAsk: (message) => ipcRenderer.invoke("aarya:ask", { message }),
 
   onTabUpdate: (callback) => {
-    ipcRenderer.on(
-      "tab-update",
-      (_, data) => callback(data)
-    );
+    ipcRenderer.on("tab-update", (_, data) => callback(data));
   },
 
   onAddressUpdate: (callback) => {
-    ipcRenderer.on(
-      "address-update",
-      (_, url) => callback(url)
-    );
+    ipcRenderer.on("address-update", (_, url) => callback(url));
   },
 
   onFocusAddressBar: (callback) => {
-    ipcRenderer.on(
-      "focus-address-bar",
-      () => callback()
-    );
+    ipcRenderer.on("focus-address-bar", () => callback());
   }
 });
